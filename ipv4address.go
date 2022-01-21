a32f0a3074da037caad99c8983333817f2dfd460
Gartenschlaeger <mail@kaisnet.de>
2022-01-21 21:10:28 +0100

    add hex and bin to ip infos

--- ipv4address.go
+++ ipv4address.go
@@ -10,12 +10,14 @@ import (
 func processIpv4Address(ipv4Address string) {
 	ip := net.ParseIP(ipv4Address)
 	if ip != nil {
-		fmt.Printf("address        %v\n", ipv4Address)
-		fmt.Printf("decimal        %v\n", iplib.IP4ToUint32(ip))
-		fmt.Printf("arpa           %v\n", iplib.IP4ToARPA(ip))
-		fmt.Printf("private        %v\n", ip.IsPrivate())
-		fmt.Printf("loopback       %v\n", ip.IsLoopback())
-		fmt.Printf("multicast      %v\n", ip.IsMulticast())
-		fmt.Printf("unspecified    %v\n", ip.IsUnspecified())
+		fmt.Printf("address         %v\n", ipv4Address)
+		fmt.Printf("arpa            %v\n", iplib.IP4ToARPA(ip))
+		fmt.Printf("is private      %v\n", ip.IsPrivate())
+		fmt.Printf("is loopback     %v\n", ip.IsLoopback())
+		fmt.Printf("is multicast    %v\n", ip.IsMulticast())
+		fmt.Printf("is unspecified  %v\n", ip.IsUnspecified())
+		fmt.Printf("dec             %v\n", iplib.IP4ToUint32(ip))
+		fmt.Printf("bin             %v\n", iplib.IPToBinaryString(ip))
+		fmt.Printf("hex             %v\n", iplib.IPToHexString(ip))
 	}
 }
